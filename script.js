424094061520376c5011c58e10dec84e7cb60b2f
YM <ymacedo05@gmail.com>
2022-06-20 22:24:53 -0300

    ...

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ function digitar(x) {
     let alvo = document.querySelector(x)
     let texto = alvo.innerHTML.split('')
 
-    console.log(alvo)
-    console.log(texto)
 
     //=================
 
     alvo.innerHTML = ''
 
     for(let i = 0; i < texto.length; i++) {
-        console.log(texto[i])
 
         setTimeout(function(){
             alvo.innerHTML += texto[i]
-        }, 100 * i)
+        }, 120 * i)
 
     }
 
     setTimeout(function(){
         digitar('#alvo')
-    },7000)
+    },6000)
 
 
 }
