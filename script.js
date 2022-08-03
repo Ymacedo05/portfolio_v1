c4f8a848728e90bb867b025fe6435a417c85f67d
YM <ymacedo05@gmail.com>
2022-08-03 11:21:37 -0300

    ...

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+/* ================= MENU ===========================*/
+let contador = 0
+
+function clickBTN() {
+
+    contador++
+    let nav = document.getElementById('nav')
+
+    if(contador == 1) {
+        
+        nav.classList.add('active')
+
+        nav.addEventListener('click', (x) => {
+            console.log(x.target.id)
+            if(x.target.id == 'nav' || x.target.className == 'fechar') {
+                nav.classList.remove('active')
+            }
+        } )
+
+    } else if (contador == 2) {
+
+        let nav = document.getElementById('nav')
+        nav.classList.remove('active')
+        contador = 0
+    }
+    
+}
+
+/* ================= EFEITO MAQUINA DE ESCREVER ================ */
 function digitar(x) {
 
 
@@ -26,3 +56,29 @@ function digitar(x) {
 
 
 digitar('#alvo')
+
+/* =================== ANIMAÇÃO SCROLL ==================== */
+
+
+function aniScroll() {
+
+    let barraTop = window.scrollY + ((window.innerHeight *3)/4)
+    let alvo = document.querySelectorAll('[data-anime]')
+
+    alvo.forEach( (elementos) => {
+        
+
+        if(barraTop > elementos.offsetTop) {
+            elementos.classList.add('active')
+        } else {
+            elementos.classList.remove('active')
+        }
+    })
+   
+}
+
+aniScroll()
+
+window.addEventListener('scroll', () => {
+    aniScroll()
+})
